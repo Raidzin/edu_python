d5f28acd15364b16ddc1b565f89c22f0fd39747a
Raidzin <60663550+Raidzin@users.noreply.github.com>
2024-04-18 16:13:48 +0300

    cool decorator 😎

--- main.py
+++ main.py
@@ -1,2 +1,53 @@
-print('Hello, Github!')
-print('Hello, Branches!')
+import time
+from random import randint
+
+# t1 = time.time()
+# time.sleep(4)
+# t2 = time.time()
+
+N = 2
+
+# def decorator(func):
+#     def wrapper():
+#         return func()
+#     return wrapper
+
+
+# def time_of(a):
+#     counter = 0
+#     for _ in range(N):
+#         time_1 = time.time()
+#         a()
+#         counter += time.time() - time_1
+#     return counter / N
+
+def time_of(func):
+    def wrapper():
+        summ = 0
+        original = None
+        for _ in range(N):
+            time_1 = time.time()
+            value = func()
+            summ += time.time() - time_1
+            if original is None:
+                original = value
+        print(summ / N)
+        return original
+
+    return wrapper
+
+
+@time_of
+def foo():
+    r = randint(0, 5)
+    time.sleep(r)
+    return r
+
+@time_of
+def main():
+    a = foo()
+    print(a)
+
+
+if __name__ == '__main__':
+    main()
